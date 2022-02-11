1a416dc875b0299db49c75a17deb9a76caf60587
js5787 <ljs950430@gmail.com>
2022-02-11 19:27:35 +0900

    prod/appspec10

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -2,10 +2,8 @@
 cd /home/ubuntu/eotd-backend
 npm install
 npm install pm2@latest -g
-sudo apt-get update
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
 sudo chmod 755 /etc/authbind/byport/80
-npm i -g @nestjs/cli
 npm run build
