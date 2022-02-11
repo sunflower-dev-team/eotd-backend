20ef7bf3e1be3f841a0613490758e09af86ef828
js5787 <ljs950430@gmail.com>
2022-02-12 02:32:06 +0900

    fix/script4

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 cd /home/ubuntu/eotd-backend
+sudo apt update
+sudo apt install nodejs npm
+nvm install v14.18.0
+nvm use v14.18.0
 npm install
 npm install pm2@latest -g
-npm install @nestjs/cli -g 
 sudo apt-get update
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
 sudo chmod 755 /etc/authbind/byport/80
+npm install @nestjs/cli -g 
 npm run build
