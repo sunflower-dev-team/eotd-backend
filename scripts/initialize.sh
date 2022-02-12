75b263b9f4503eecda186e62f2e2c52a894a8fa1
js5787 <ljs950430@gmail.com>
2022-02-12 19:29:42 +0900

    prod/success

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -13,7 +13,7 @@ npm install pm2@latest -g
 sudo apt-get update
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
-sudo chown ubuntu /etc/authbind/byport/80
+sudo chown root /etc/authbind/byport/80
 sudo chmod 755 /etc/authbind/byport/80
 npm install @nestjs/cli -g 
 npm run build
