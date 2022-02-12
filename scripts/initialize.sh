87230684cea48668db6d9f899e6bcf0356629034
js5787 <ljs950430@gmail.com>
2022-02-12 20:23:39 +0900

    prod/success

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 cd /home/ubuntu/eotd-backend
-sudo apt update
+apt update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 npm install node
-sudo apt install npm -y
+apt install npm -y
 nvm install v14.18.0
 nvm use v14.18.0
 npm install
-sudo npm install pm2@latest -g
-sudo apt-get update
-sudo apt-get install authbind
-sudo touch /etc/authbind/byport/80
-sudo chown ubuntu /etc/authbind/byport/80
-sudo chmod 755 /etc/authbind/byport/80
+npm install pm2@latest -g
+apt-get update
+apt-get install authbind
+touch /etc/authbind/byport/80
+chown ubuntu /etc/authbind/byport/80
+chmod 755 /etc/authbind/byport/80
 npm install @nestjs/cli -g 
 npm run build
