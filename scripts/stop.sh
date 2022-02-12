278d126005aad3bd1a31435def7a81b84e72cfdc
js5787 <ljs950430@gmail.com>
2022-02-12 19:48:09 +0900

    prod/success

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/eotd-backend
-pm2 stop dist/main.js 2> /dev/null || true
-pm2 delete dist/main.js 2> /dev/null || true
+sudo pm2 stop dist/main.js 2> /dev/null || true
+sudo pm2 delete dist/main.js 2> /dev/null || true
