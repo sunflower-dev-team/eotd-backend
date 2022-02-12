50092dd054f2afb17086b163f96e4d8f5be2b88e
js5787 <ljs950430@gmail.com>
2022-02-12 20:16:32 +0900

    prod/success

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -10,7 +10,6 @@ nvm install v14.18.0
 nvm use v14.18.0
 npm install
 npm install pm2@latest -g
-npm install pm2@3 -g
 sudo apt-get update
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
